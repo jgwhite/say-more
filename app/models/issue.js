89ab9069c3dc49d11417314e626ec1f0867cbeba
Jamie White <jamie@jgwhite.co.uk>
2018-01-23 11:43:13 +0000

    Allow issue models to take arrays for assignees, labels

--- app/models/issue.js
+++ app/models/issue.js
@@ -4,8 +4,8 @@ import attr from 'ember-data/attr';
 export default Model.extend({
   title: attr('string'),
   body: attr('string'),
-  assignees: attr('string'),
-  labels: attr('string'),
+  assignees: attr(),
+  labels: attr(),
   project: attr('string'),
   milestone: attr('string')
 });
