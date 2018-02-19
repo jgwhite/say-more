a3f80cab8c8c2b28842ff1cb5408426ab7dae163
Jamie White <jamie@jgwhite.co.uk>
2018-02-19 18:57:33 +0000

    Implement tabbibility check

--- tests/helpers/click.js
+++ tests/helpers/click.js
@@ -3,16 +3,22 @@ import { click, findAll } from '@ember/test-helpers';
 export default function clickByLabel(text) {
   let element = findElement(text);
 
+  if (!element) {
+    throw new Error(`Could not find a button containing "${text}"`);
+  }
+
+  let tabs = calculateTabsTo(element);
+
+  if (tabs < 0) {
+    throw new Error(`The user would have to tab backwards to reach the button containing "${text}"`);
+  }
+
   return click(element);
 }
 
 function findElement(text) {
   let selector = 'button,a[href],[role="button"]';
   let element = findAll(selector).find(matches(text));
-
-  if (!element) {
-    throw new Error(`Could not find a button containing "${text}"`);
-  }
 
   return element;
 }
@@ -34,4 +40,14 @@ function matchesTitle(element, text) {
 function matchesAriaLabel(element, text) {
   let ariaLabel = element.getAttribute('aria-label');
   return ariaLabel && ariaLabel.includes(text);
+}
+
+function calculateTabsTo(targetElement) {
+  let { activeElement } = document;
+  // TODO: Make this less naive
+  let tabbables = findAll('*').filter(e => e.tabIndex >= 0);
+  let activeIndex = tabbables.indexOf(activeElement);
+  let targetIndex = tabbables.indexOf(targetElement);
+
+  return targetIndex - activeIndex;
 }
